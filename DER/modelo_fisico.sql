baea772adff211d4f6284ad9cc0f468dcebc214e
PedroBSanchez <pedro_b47@hotmail.com>
2021-06-09 19:53:06 -0300

    Implementação de auto_increment nas tabelas

--- DER/modelo_fisico.sql
+++ DER/modelo_fisico.sql
@@ -1,15 +1,15 @@
 create table clientes (
-    id_cliente int,
-    nome varchar2(70),
+    id_cliente int not null auto_increment,
+    nome char(70),
     data_nascimento date,
-    sexo varchar2(30),
-    cpf varchar2(11),
+    sexo char(30),
+    cpf char(11),
     constraint pk_clientes primary key (id_cliente)
 );
 
 create table telefones (
-    id_telefone int,
-    numero varchar2(15),
+    id_telefone int not null auto_increment,
+    numero char(15),
     id_cliente int,
     constraint pk_telefones primary key (id_telefone),
     constraint fk_telefonesClientes foreign key (id_cliente) references clientes(id_cliente)
@@ -17,11 +17,11 @@ create table telefones (
 );
 
 create table enderecos (
-    id_endereco int,
-    cep varchar2(10),
-    cidade varchar2(50),
-    bairro varchar2(50),
-    logradouro varchar2(70),
+    id_endereco int not null auto_increment,
+    cep char(10),
+    cidade char(50),
+    bairro char(50),
+    logradouro char(70),
     numero int,
     id_cliente int,
     constraint pk_enderecos primary key (id_endereco),
@@ -30,8 +30,8 @@ create table enderecos (
 
 
 create table produtos (
-    id_produto int,
-    nome varchar2(70),
+    id_produto int not null auto_increment,
+    nome char(70),
     vl_venda float,
     vl_custo float,
     quantidade_estoque int,
@@ -40,7 +40,7 @@ create table produtos (
 
 
 create table pedidos (
-    id_pedido int,
+    id_pedido int not null auto_increment,
     data_pedido date,
     id_cliente int,
     constraint pk_pedidos primary key (id_pedido),
