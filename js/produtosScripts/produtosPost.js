395bb3b39c7f60268af2d273ae8e9e3fe8666cbb
PedroBSanchez <pedro_b47@hotmail.com>
2021-06-11 16:46:27 -0300

    Implementação de método POST com simulação de API

---
+++ js/produtosScripts/produtosPost.js
@@ -0,0 +1,35 @@
+function fazPost(url, body) {
+    var request = new XMLDocument();
+    request.open("POST", url, true);
+    request.setRequestHeader("Content-type", "application/json");
+    request.send(JSON.stringify(body));
+    request.onload = function(){
+        console.log(this.responseText);
+
+    }
+
+    return request.responseText;
+}
+
+function cadastrarProduto(){
+    event.preventDefault();
+    //var url = "api" url para enviar para api
+    var nome = document.getElementById("produtoNome").value;
+    var vlCusto = document.getElementById("produtoVlCusto").value;
+    var vlVenda = document.getElementById("produtoVlVenda").value;
+    var qtdeEstoque = document.getElementById("produtoQtdeEstoque").value;
+    
+    body = {
+        "nome" : nome,
+        "vl_custo" : vlCusto,
+        "vl_venda" : vlVenda,
+        "quantidade_estoque" : qtdeEstoque
+    };
+
+    console.log(body);
+
+    //fazPost(url, body) "Utilizada quando api estiver pronta"
+
+
+
+}
